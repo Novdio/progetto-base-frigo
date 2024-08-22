9b43f5afd48108945ad939d575f9aba9fb4221c7
Alessio Nordio <ale0909.nordio@gmail.com>
2024-08-22 14:44:26 +0200

    Notifiche webSocket, prova ma manca front credo

--- src/main/java/com/generation/progetto_finale/auth/dto/UserAdditionalInfoDTO.java
+++ src/main/java/com/generation/progetto_finale/auth/dto/UserAdditionalInfoDTO.java
@@ -20,21 +20,7 @@ public class UserAdditionalInfoDTO {
 
     private List<String> diet;
 
-    // Aggiungere BMI e IBW
-    public Double getIdealWeight() {
-        switch (sex) {
-            case "M":
-                return 50 + (0.91 * (height - 152.4));
-            case "F":
-                return 45.5 + (0.91 * (height - 152.4));
-            default:
-                throw new RuntimeException();
+    
 
-        }
-    }
-
-    public Double getBmi()
-    {
-        return weight/(Math.pow(height, 2));
-    }
+    
 }
