5abac01db59507227ad17913880a078dd2c9611a
Alice Ibba <alice.ibba99@outlook.it>
2024-08-26 11:03:17 +0200

    cambio

--- src/main/java/com/generation/progetto_finale/controller/CalendarEventController.java
+++ src/main/java/com/generation/progetto_finale/controller/CalendarEventController.java
@@ -2,18 +2,16 @@ package com.generation.progetto_finale.controller;
 
 import java.util.Optional;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.progetto_finale.auth.dto.CalendarEventDTO;
 import com.generation.progetto_finale.auth.dto.mappers.CalendarEventService;
@@ -22,6 +20,7 @@ import com.generation.progetto_finale.auth.model.UserAdditionalInfo;
 import com.generation.progetto_finale.auth.repository.CalendarEventRepository;
 import com.generation.progetto_finale.auth.repository.UserAdditionalInfoRepository;
 import com.generation.progetto_finale.auth.repository.UserRepository;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/calendar")
@@ -38,19 +37,16 @@ public class CalendarEventController {
 
     @GetMapping("{id}")
     public CalendarEventDTO getCalendarEventInfo(@PathVariable Integer id) {
-        return cRepo.findById(id)
-                .map(cServ::toDTO)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Event not found"));
+        return cServ.toDTO(cRepo.findById(id).get());
     }
 
-    @PostMapping
-    public CalendarEventDTO formCalendarEvent(@RequestBody CalendarEventDTO DTO) {
+    @PostMapping("{id}")
+    public CalendarEventDTO formCalendarEvent(@RequestBody CalendarEventDTO DTO, @PathVariable int id) {
 
         CalendarEvent calendarDaCreare = cServ.toEntity(DTO);
-        Optional<UserAdditionalInfo> u = uRepo.findById(DTO.getId());
-
+        Optional<UserAdditionalInfo> u = uRepo.findById(id);
         if (u.isEmpty())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+            throw new RuntimeErrorException(new Error("No trovato capo"));
 
         calendarDaCreare.setUser(u.get());
         calendarDaCreare = cRepo.save(calendarDaCreare);
@@ -59,22 +55,22 @@ public class CalendarEventController {
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<Void> deleteCalendarEvent(@PathVariable int id) {
+    public void deleteCalendarEvent(@PathVariable int id) {
+
         Optional<CalendarEvent> calendarDaEliminare = cRepo.findById(id);
         if (calendarDaEliminare.isEmpty())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Event not found");
-
+            throw new RuntimeErrorException(new Error("Non si può cancellare, non esiste"));
         cRepo.delete(calendarDaEliminare.get());
-        return ResponseEntity.noContent().build();
     }
 
     @PutMapping("{id}")
     public CalendarEventDTO modifyCalendarEvent(@PathVariable int id, @RequestBody CalendarEventDTO dto) {
+
         CalendarEvent calendarDaModificare = cServ.toEntity(dto);
         Optional<UserAdditionalInfo> u = uRepo.findById(id);
 
         if (u.isEmpty())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+            throw new RuntimeErrorException(new Error("non posso modificare"));
 
         calendarDaModificare.setUser(u.get());
         calendarDaModificare = cRepo.save(calendarDaModificare);
