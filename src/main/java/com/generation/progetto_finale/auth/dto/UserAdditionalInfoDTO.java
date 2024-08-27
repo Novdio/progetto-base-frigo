f98a173eced4b918a3fc13eee727a25acfaca1d9
Claudia Amoruso <claudia.amoruso00@gmail.com>
2024-08-27 09:46:27 +0200

    gg

--- src/main/java/com/generation/progetto_finale/auth/dto/UserAdditionalInfoDTO.java
+++ src/main/java/com/generation/progetto_finale/auth/dto/UserAdditionalInfoDTO.java
@@ -19,22 +19,4 @@ public class UserAdditionalInfoDTO {
     private Double height;
 
     private List<String> diet;
-
-    // Aggiungere BMI e IBW
-    public Double getIdealWeight() {
-        switch (sex) {
-            case "M":
-                return 50 + (0.91 * (height - 152.4));
-            case "F":
-                return 45.5 + (0.91 * (height - 152.4));
-            default:
-                throw new RuntimeException();
-
-        }
-    }
-
-    public Double getBmi()
-    {
-        return weight/(Math.pow(height, 2));
-    }
 }
